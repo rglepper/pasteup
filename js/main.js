527fde38ddecf6dca5cd9a93f314758de2443a8d
Andy Hume <andyhume@gmail.com>
2012-03-26 16:29:22 +0100

    Added CSS updating detection values.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 require(["detect/detect"], function(detect) {
-	document.querySelector('[data-feature="layout"]').innerHTML = detect.getLayoutMode();
-	document.querySelector('[data-feature="bandwidth"]').innerHTML = detect.getConnectionSpeed();
-	document.querySelector('[data-feature="battery"]').innerHTML = (detect.getBatteryLevel() === 1) ? "full" : detect.getBatteryLevel();
-	document.querySelector('[data-feature="retina"]').innerHTML = (detect.getPixelRatio() === 2) ? "true" : "false";
+
+	function doDetect() {
+		document.querySelector('[data-feature="layout"]').innerHTML = detect.getLayoutMode();
+		document.querySelector('[data-feature="bandwidth"]').innerHTML = detect.getConnectionSpeed();
+		document.querySelector('[data-feature="battery"]').innerHTML = (detect.getBatteryLevel() === 1) ? "full" : detect.getBatteryLevel();
+		document.querySelector('[data-feature="retina"]').innerHTML = (detect.getPixelRatio() === 2) ? "true" : "false";
+	}
+
+	window.addEventListener("resize", function() {
+		doDetect();
+	}, false);
+
+	doDetect();
+
 });
